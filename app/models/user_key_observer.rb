5dd44fb8053a2ed3c6e3132d20c5e865548bc488
Carl P. Corliss <rabbitt@gmail.com>
2011-05-18 11:33:54 -0400

    fix bug with disabling user where sua.server ends up being nil

--- app/models/user_key_observer.rb
+++ app/models/user_key_observer.rb
@@ -2,45 +2,47 @@ class UserKeyObserver < ActiveRecord::Observer
     @public_key_changed = false
 
     def after_create(user_key)
-        self.class.add_user_key_to_servers(user_key) if not user_key.public_key.blank? and user_key.user.enabled?
+        self.add_user_key_to_servers(user_key) if not user_key.public_key.blank? and user_key.user.enabled?
     end
 
     def before_destroy(user_key)
-        self.class.delete_user_key_from_servers(user_key)
+        self.delete_user_key_from_servers(user_key)
     end
 
     def before_update(user_key)
         @public_key_changed = user_key.public_key_changed?
-        self.class.delete_user_key_from_servers(user_key) if @public_key_changed
+        self.delete_user_key_from_servers(user_key) if @public_key_changed
     end
 
     def after_update(user_key)
-        self.class.add_user_key_to_servers(user_key) if @public_key_changed and not user_key.public_key.blank? and user_key.user.enabled?
+        self.add_user_key_to_servers(user_key) if @public_key_changed and not user_key.public_key.blank? and user_key.user.enabled?
     end
 
     def self.add_user_key_to_servers(user_key)
         suas = ServerUserAccess.find_all_by_user_id(user_key.user_id, :include => :server)
-	unless suas.nil?
-	    suas.each do |sua|
-		sua.server.add_user_key(user_key, sua.server_user)
-	    end
-	end
-	servers = Server.find_all_by_user(user_key.user_id)
-	servers.each do |server|
-	    server.add_user_key(user_key, 'root')
-	end
+        unless suas.nil?
+            suas.each do |sua|
+                # TODO: handle the case of sua.server being nil better
+                sua.server.add_user_key(user_key, sua.server_user) rescue next
+            end
+        end
+        servers = Server.find_all_by_user(user_key.user_id)
+        servers.each do |server|
+            server.add_user_key(user_key, 'root')
+        end
     end
 
     def self.delete_user_key_from_servers(user_key)
         suas = ServerUserAccess.find_all_by_user_id(user_key.user_id, :include => :server)
         unless suas.nil?
-	    suas.each do |sua|
-		sua.server.delete_user_key(user_key, sua.server_user)
-	    end
-	end
-	servers = Server.find_all_by_user(user_key.user_id)
-	servers.each do |server|
-	    server.delete_user_key(user_key, 'root')
-	end
+            suas.each do |sua|
+                # TODO: handle the case of sua.server being nil better
+                sua.server.delete_user_key(user_key, sua.server_user) rescue next
+            end
+        end
+        servers = Server.find_all_by_user(user_key.user_id)
+        servers.each do |server|
+            server.delete_user_key(user_key, 'root')
+        end
     end
 end
