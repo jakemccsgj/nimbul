a065935a77c6a0c10a82e29646d9b7c4bda119e9
Vadim Jelezniakov <vadimj@gmail.com>
2011-07-28 10:46:37 -0400

    added support for vm price (back-end and ui)

--- app/models/vm_price.rb
+++ app/models/vm_price.rb
@@ -5,6 +5,7 @@ class VmPrice < ActiveRecord::Base
     belongs_to :instance_vm_type
     belongs_to :vm_os_type
 
-    validates_presence_of :api_name, :name
-    validates_uniqueness_of :api_name, :name, :scope => :provider_id
+    validates_presence_of :price, :price_unit, :price_period
+    validates_numericality_of :price
+    validates_uniqueness_of [:vm_price_type_id, :region_id, :instance_vm_type_id, :vm_os_type_id], :scope => :provider_id
 end
