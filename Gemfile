8ac644e0447b76ca1aa20cbb10e60b6ad7b902da
Chris DiMartino <vwchris@gmail.com>
2011-09-29 09:38:31 -0400

    Changes to make Nimubl Bundler compatible

---
+++ Gemfile
@@ -0,0 +1,37 @@
+source :gemcutter
+
+gem 'rails', '~> 2.3.0'
+gem 'Ruby-MemCache', :require => 'memcache'
+gem 'SysVIPC'
+gem 'aasm'
+gem 'carrot'
+gem 'chronic'
+gem 'emissary'
+gem 'fastthread'
+gem 'mysql'
+gem 'net-sftp', :require => 'net/sftp'
+gem 'net-ssh', :require => 'net/ssh'
+gem 'ruby-openid'
+gem 'rufus-scheduler'
+gem 'work_queue'
+git 'https://github.com/nimbul/cloudmaster.git' do
+  gem 'cloudmaster'
+end
+git 'https://github.com/justinfrench/formtastic.git', :branch => '1.2-stable' do
+  gem 'formtastic', :branch => '1.2-stable'
+end
+git 'https://github.com/Velir/open_id_authentication.git', :tag => 'v1.0.0' do
+  gem 'open_id_authentication'
+end
+group :development do
+end
+
+group :test do
+  gem 'rspec'
+  gem 'autotest'
+  gem 'ZenTest'
+  gem 'cucumber'
+end
+
+group :production do
+end
--- config/environment.rb
+++ config/environment.rb
@@ -1,15 +1,17 @@
 # Be sure to restart your server when you modify this file
-require 'yaml'
+#require 'rubygems'
+#require 'bundler/setup'
+#Bundler.require(:default)
 
-# for more info, see: http://ozmm.org/posts/try.html 
+# for more info, see: http://ozmm.org/posts/try.html
 #
 # dirty little helper method that protects us from nils
-# 
-# used like so: 
-# 
+#
+# used like so:
+#
 #   Person.find_by_email(nonexistant_email).try(:name)
 #
-#   This will return nil if find didn't find the record or 
+#   This will return nil if find didn't find the record or
 #   the name if it found the record
 
 class Object
@@ -21,14 +23,8 @@ end
 class ServiceWithoutActiveInstance < Exception
 end
 
-
-# Uncomment below to force Rails into production mode when
-# you don't control web/app server and can't set it the proper way
-ENV['RAILS_ENV'] ||= 'production'
-
 # Specifies gem version of Rails to use when vendor/rails is not present
-#RAILS_GEM_VERSION = '2.1.0' unless defined? RAILS_GEM_VERSION
-RAILS_GEM_VERSION = '2.2.2' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.14' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -52,47 +48,15 @@ Rails::Initializer.run do |config|
   if ENV['DAEMON_SCRIPTLET']
     config.frameworks = [ :active_record, :action_mailer ]
   end
-  
-  # Specify gems that this application depends on.
-  # They can then be installed with "rake gems:install" on new installations.
-  # config.gem "bj"
-  # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
-  # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "rufus-scheduler"
-  config.gem "chronic"
-  config.gem 'justinfrench-formtastic', :lib => 'formtastic', :source => 'http://gems.github.com'
-  config.gem 'work_queue', :source => 'http://gems.github.com'
-  config.gem 'carrot'
-  config.gem 'emissary'
 
-  # Only load the plugins named here, in the order given. By default, all plugins
-  # in vendor/plugins are loaded in alphabetical order.
-  # :all can be used as a placeholder for all plugins not explicitly named
-  # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
-  # Add additional load paths for your own custom dirs
-  # config.load_paths += %W( #{RAILS_ROOT}/extras )
-
-  # Force all environments to use the same logger level
-  # (by default production uses :info, the others :debug)
-  # config.log_level = :debug
-
-  # Make Time.zone default to the specified zone, and make Active Record store time values
-  # in the database in UTC, and return them converted to the specified local zone.
-  # Run "rake -D time" for a list of tasks for finding time zone names. Uncomment to use default local time.
-  #config.time_zone = 'UTC'
   config.time_zone = 'Eastern Time (US & Canada)'
-
-#    config.cache_store = :mem_cache_store
-#    config.action_controller.perform_caching = true
-
 
   # Your secret key for verifying cookie session data integrity.
   # If you change this key, all old sessions will become invalid!
   # Make sure the secret is at least 30 characters and all random,
   # no regular words or you'll be exposed to dictionary attacks.
   config.action_controller.session = {
-    :session_key => APP_CONFIG['settings']['session_key'],
+    :key => APP_CONFIG['settings']['session_key'],
     :secret  => APP_CONFIG['settings']['secret']
   }
 
@@ -122,7 +86,7 @@ Rails::Initializer.run do |config|
 
   # Helper type for select boxes with groups and filter
   GroupLabelValueFilter = Struct.new(:group, :label, :value, :filter)
-  
+
   # Helper type to JSON Error handling
   ModelError = Struct.new(:model,:error)
 
@@ -133,32 +97,32 @@ Rails::Initializer.run do |config|
   # <NOTE>: We don't support micro until we support EBS-based root devices because micros are only supported using those
   # EC2_INSTANCE_TYPES = [ 't1.micro', 'm1.small', 'm1.large', 'm1.xlarge', 'c1.medium', 'c1.xlarge' ]
   #EC2_INSTANCE_TYPES = [ 'm1.small', 'm1.large', 'm1.xlarge', 'c1.medium', 'c1.xlarge' ]
-  EC2_SERVER_USERS = [ 'root', 'dev', 'logpoll' ]
-  PUBLISH_EVERY_VALUES = [ 0, 10, 20, 30, 60, 120, 300, 600, 1800, 3600 ]
-  IN_MESSAGE_STATES = [ 'new', 'processed' ]
-  OUT_MESSAGE_STATES = [ 'pending', 'sent' ]
-  PROTOCOLS_LC = [ 'tcp', 'udp', 'icmp' ]
-  PROTOCOLS = PROTOCOLS_LC.map{ |p| LabelValue.new(p, p.upcase) }
-  VOLUME_CLASSES = [ 'Volume', 'Snapshot', 'AnotherServer' ]
-  RUN_EVERY_UNITS = [ 'minutes', 'hours', 'days', 'weeks' ]
-    AS_TRIGGER_MEASURE_NAMES =  {
-		'CPUUtilization' => [ 'Percent' ],
-		'NetworkIn' => [ 'Bytes', 'Bytes/Second', 'Bits', 'Bits/Second' ],
-		'NetworkOut' => [ 'Bytes', 'Bytes/Second', 'Bits', 'Bits/Second' ],
-		'DiskWriteOps' => [ 'Count', 'Count/Second' ],
-		'DiskReadOps' => [ 'Count', 'Count/Second' ],
-		'DiskWriteBytes' => ['Bytes', 'Bytes/Second', 'Bits', 'Bits/Second' ],
-		'DiskReadBytes' => [ 'Bytes', 'Bytes/Second', 'Bits','Bits/Second' ],
-	}
-    AS_TRIGGER_STATISTICS =  [ 'Average','Minimum','Maximum','Sum' ]
-    AS_TRIGGER_PERIOD_UNITS = [ 'minutes', 'hours', 'days' ]
-    AS_TRIGGER_BREACH_DURATION_UNITS = [ 'minutes', 'hours', 'days' ]
-    AS_TRIGGER_THRESHOLD_ACTIONS = [ 'increase', 'decrease' ]
-    AS_TRIGGER_BREACH_SCALE_INCREMENT_UNITS = [ '%', 'instances' ]
-    SERVER_VOLUME_MOUNT_TYPES_ARRAY = [ 'Mount Volume Mount Type', 'Restore Snapshot Mount Type', 'Restore Latest Snapshot Mount Type' ]
-	SERVER_VOLUME_MOUNT_TYPES = SERVER_VOLUME_MOUNT_TYPES_ARRAY.map{ |t| LabelValue.new(t.gsub('Mount Type',''), t.gsub(' ','')) }
+  EC2_SERVER_USERS                        = [ 'root', 'dev', 'logpoll' ]
+  PUBLISH_EVERY_VALUES                    = [ 0, 10, 20, 30, 60, 120, 300, 600, 1800, 3600 ]
+  IN_MESSAGE_STATES                       = [ 'new', 'processed' ]
+  OUT_MESSAGE_STATES                      = [ 'pending', 'sent' ]
+  PROTOCOLS_LC                            = [ 'tcp', 'udp', 'icmp' ]
+  PROTOCOLS                               = PROTOCOLS_LC.map{ |p| LabelValue.new(p, p.upcase) }
+  VOLUME_CLASSES                          = [ 'Volume', 'Snapshot', 'AnotherServer' ]
+  RUN_EVERY_UNITS                         = [ 'minutes', 'hours', 'days', 'weeks' ]
+  AS_TRIGGER_MEASURE_NAMES                = {
+    'CPUUtilization'  => [ 'Percent' ],
+    'NetworkIn'       => [ 'Bytes', 'Bytes/Second', 'Bits', 'Bits/Second' ],
+    'NetworkOut'      => [ 'Bytes', 'Bytes/Second', 'Bits', 'Bits/Second' ],
+    'DiskWriteOps'    => [ 'Count', 'Count/Second' ],
+    'DiskReadOps'     => [ 'Count', 'Count/Second' ],
+    'DiskWriteBytes'  => ['Bytes', 'Bytes/Second', 'Bits', 'Bits/Second' ],
+    'DiskReadBytes'   => [ 'Bytes', 'Bytes/Second', 'Bits','Bits/Second' ],
+  }
+  AS_TRIGGER_STATISTICS                   = [ 'Average','Minimum','Maximum','Sum' ]
+  AS_TRIGGER_PERIOD_UNITS                 = [ 'minutes', 'hours', 'days' ]
+  AS_TRIGGER_BREACH_DURATION_UNITS        = [ 'minutes', 'hours', 'days' ]
+  AS_TRIGGER_THRESHOLD_ACTIONS            = [ 'increase', 'decrease' ]
+  AS_TRIGGER_BREACH_SCALE_INCREMENT_UNITS = [ '%', 'instances' ]
+  SERVER_VOLUME_MOUNT_TYPES_ARRAY         = [ 'Mount Volume Mount Type', 'Restore Snapshot Mount Type', 'Restore Latest Snapshot Mount Type' ]
+  SERVER_VOLUME_MOUNT_TYPES               = SERVER_VOLUME_MOUNT_TYPES_ARRAY.map{ |t| LabelValue.new(t.gsub('Mount Type',''), t.gsub(' ','')) }
 
-    PER_TIME_UNITS = [ 'hour', 'day', 'month', '1 year', '3 years' ]
+  PER_TIME_UNITS                          = [ 'hour', 'day', 'month', '1 year', '3 years' ]
 
   STARTUP_SCRIPT_PACKAGERS = [
     { :name => 'nimbul', :description => "Combines Provider Account's, Cluster's and Server's Variables and Startup Scripts. Minifies and compresses to create the final User Data. The resulting User Data requires an NYTd-built AMI to run successfully." },
@@ -174,10 +138,6 @@ Rails::Initializer.run do |config|
 
   LDNS_REGISTRY_PATH = RAILS_ROOT + '/tmp/cache'
   LDNS_HOSTFILE_PATH = RAILS_ROOT + '/tmp/cache/ldns-hosts'
-
-  # Tell ActiveRecord not to include the root while serializing to JSON - needed by better-edit-in-place plugin
-  #ActiveRecord::Base.include_root_in_json = false
 end
 
-CACHE = MemCache.new('127.0.0.1')
-#require 'validates_uri_existence_of'
+CACHE = MemCache.new('memcached.lcl')
