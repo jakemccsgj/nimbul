5d3a6ac9d070d78f697d06cba1d2eaf1a00d3a3e
Chris DiMartino <vwchris@gmail.com>
2011-09-29 09:25:14 -0400

    Renaming controller to more appropriate name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,9 +19,9 @@ class ApplicationController < ActionController::Base
 
 	# See ActionController::RequestForgeryProtection for details
 	# Uncomment the :secret if you're not using the cookie session store
-	protect_from_forgery :secret => APP_CONFIG['settings']['secret']
+	protect_from_forgery #:secret => APP_CONFIG['settings']['secret']
 
-	# See ActionController::Base for details
+	# See ::Base for details
 	# Uncomment this to filter the contents of submitted sensitive data parameters
 	# from your application log (in this case, all fields with names like "password").
 	filter_parameter_logging :password, :password_confirmation, :old_password
