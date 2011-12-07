68547bddee77798f7a28483aed7b24b8f6b0132c
Chris DiMartino <christopher.dimartino@nytimes.com>
2011-12-07 18:17:04 -0500

    Updates to support curling userdata

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,64 +1,66 @@
+require 'erb'
+
 class Server < BaseModel
   PARAMETER_PRIORITY = [
     ServerProfileRevisionParameter,
     ClusterParameter,
     ProviderAccountParameter
   ]
-      
-    behavior :service
 
-    service_parent_relationship :cluster
-    service_child_relationship :none
+  behavior :service
 
-    belongs_to :cluster, :counter_cache => true, :include => :provider_account
-    belongs_to :server_profile_revision
-  
-    has_and_belongs_to_many :security_groups, :order => :name, :uniq => true
-    has_many :instances, :dependent => :nullify
+  service_parent_relationship :cluster
+  service_child_relationship :none
 
-    has_many :server_user_accesses
-    has_many :users, :through => :server_user_accesses
+  belongs_to :cluster, :counter_cache => true, :include => :provider_account
+  belongs_to :server_profile_revision
 
-    has_and_belongs_to_many :dns_hostnames, :join_table => :dns_hostname_assignments, :select => 'dns_hostnames.*'
-    has_many :dns_hostname_assignments, :dependent => :destroy
-    
-    has_many :tasks, :as => :taskable, :dependent => :destroy
-    has_one :default_resource_bundle, :class_name => 'ResourceBundle', :conditions => { :is_default => true }, :include => [ :zone, :server_resources, :addresses, :volumes, :instance ]
-    has_many :resource_bundles, :dependent => :destroy, :order => 'position', :include => [ :zone, :server_resources, :addresses, :volumes, :instance ]
-    has_many :zones, :through => :resource_bundles, :order => :name, :uniq => true, :readonly => true
-    has_many :addresses, :through => :resource_bundles, :uniq => true
-    has_many :volumes, :through => :resource_bundles, :uniq => true
-    
-    # auditing
-    has_many :logs, :class_name => 'AuditLog', :dependent => :nullify
-    has_many :audit_logs, :as => :auditable, :dependent => :nullify
+  has_and_belongs_to_many :security_groups, :order => :name, :uniq => true
+  has_many :instances, :dependent => :nullify
 
-    validates_presence_of :name
-    validates_uniqueness_of :name, :scope => :cluster_id, :message => 'there is already a Server with this name in the Cluster'
+  has_many :server_user_accesses
+  has_many :users, :through => :server_user_accesses
 
-    after_save :save_server_user_accesses, :update_instances
-    before_destroy :update_instances
+  has_and_belongs_to_many :dns_hostnames, :join_table => :dns_hostname_assignments, :select => 'dns_hostnames.*'
+  has_many :dns_hostname_assignments, :dependent => :destroy
 
-    attr_accessor :should_destroy, :status_message
+  has_many :tasks, :as => :taskable, :dependent => :destroy
+  has_one :default_resource_bundle, :class_name => 'ResourceBundle', :conditions => { :is_default => true }, :include => [ :zone, :server_resources, :addresses, :volumes, :instance ]
+  has_many :resource_bundles, :dependent => :destroy, :order => 'position', :include => [ :zone, :server_resources, :addresses, :volumes, :instance ]
+  has_many :zones, :through => :resource_bundles, :order => :name, :uniq => true, :readonly => true
+  has_many :addresses, :through => :resource_bundles, :uniq => true
+  has_many :volumes, :through => :resource_bundles, :uniq => true
 
-    include TrackChanges # must follow any before filters
+  # auditing
+  has_many :logs, :class_name => 'AuditLog', :dependent => :nullify
+  has_many :audit_logs, :as => :auditable, :dependent => :nullify
 
-    # overwriting this method to make sure there is only one default bundle
+  validates_presence_of :name
+  validates_uniqueness_of :name, :scope => :cluster_id, :message => 'there is already a Server with this name in the Cluster'
+
+  after_save :save_server_user_accesses, :update_instances
+  before_destroy :update_instances
+
+  attr_accessor :should_destroy, :status_message
+
+  include TrackChanges # must follow any before filters
+
+  # overwriting this method to make sure there is only one default bundle
     def default_resource_bundle=(resource_bundle)
         resource_bundles.update_all( :is_default => false )
         resource_bundles.detect{ |rb| rb.id == resource_bundle.id}.update_attribute(:is_default, true)
     end
-    
+
     def next_available_resource_bundle(zone=nil)
         rb = self.resource_bundles.detect{ |rb| rb.instance_id.nil? and ( zone.nil? or rb.zone_id == zone.id ) }
         rb = self.default_resource_bundle if rb.nil?
         return rb
     end
-    
+
     def has_resource_bundles?
         !resource_bundles.empty?
     end
-    
+
     # get count of available resource bundles, fill out with default rb if necessary
     def available_resource_bundles(count)
         rbs = self.resource_bundles.collect{ |rb| rb if rb.instance_id.blank? }.compact
@@ -67,7 +69,7 @@ class Server < BaseModel
         end
         return rbs
     end
-    
+
     def available_resources(zone_id=nil)
         # grab ids of all addresses that have been allocated for this server
         allocated_address_ids = []
@@ -81,7 +83,7 @@ class Server < BaseModel
         s = cluster.snapshots
         yield z, a, v, s
     end
-    
+
     def available_zones(zone_id=nil)
         available_resources do |z,a,v,s|
             z
@@ -93,24 +95,24 @@ class Server < BaseModel
             a
         end
     end
-    
+
     def available_volumes(zone_id=nil)
         available_resources do |z,a,v,s|
             v
         end
     end
-    
+
     def available_snapshots(zone_id=nil)
         available_resources do |z,a,v,s|
             s
         end
     end
-    
+
     def can_use_more_of?(resource_bundle_type)
         return true if resource_bundle_type == 'ResourceBundle'
         return false
     end
-    
+
     def should_destroy?
         should_destroy.to_i == 1
     end
@@ -120,13 +122,13 @@ class Server < BaseModel
         return false if ServerImage.find_by_image_id(self.image_id).try(:location).nil?
         return true
     end
-    
+
     # has bare minimum attributes set allowing for start of instances
     def startable?
         return false if self.key_name.blank? || self.instance_type.blank? || self.image_id.blank? || self.security_groups.nil?
         return true
     end
-    
+
     def startup_script_packager
         return nil unless server_profile_revision
         server_profile_revision.startup_script_packager
@@ -136,7 +138,7 @@ class Server < BaseModel
         return '' unless self.server_profile_revision
         self.server_profile_revision.startup_script
     end
-    
+
     def server_parameters
         return [] unless self.server_profile_revision
         self.server_profile_revision.server_profile_revision_parameters
@@ -197,7 +199,7 @@ class Server < BaseModel
   def get_parameter(name)
     parameters[name].value rescue nil
   end
-  
+
   #
   # Retreives the combined parameter list of the Provider Account, Cluster and this Server
   # and provides a [] access method which retreives keys based on Parameter Class Priority
@@ -226,7 +228,7 @@ class Server < BaseModel
     EOS
     _parameters
   end
-  
+
     def save_server_user_accesses
         server_user_accesses.each do |c|
             if c.should_destroy?
@@ -236,7 +238,7 @@ class Server < BaseModel
             end
         end
     end
-  
+
     def self.search_by_provider_account(provider_account, search, page, extra_joins, extra_conditions, sort=nil, filter=nil, include=nil)
         joins = [
             'INNER JOIN clusters ON clusters.id = servers.cluster_id',
@@ -248,23 +250,23 @@ class Server < BaseModel
             conditions[0] << ' AND ' + extra_conditions[0];
             conditions << extra_conditions[1..-1]
         end
-  
+
         search(search, page, joins, conditions, sort, filter, include)
     end
 
     def self.search_by_cluster(cluster, search, page, extra_joins, extra_conditions, sort=nil, filter=nil, include=nil)
         joins = [] + [extra_joins].flatten.compact
-        
+
         conditions = [ 'cluster_id = ?', (cluster.is_a?(Cluster) ? cluster.id : cluster) ]
         unless extra_conditions.blank?
             extra_conditions = [ extra_conditions ].flatten
             conditions[0] << ' AND ' + extra_conditions[0];
             conditions << extra_conditions[1..-1]
         end
-        
+
         search(search, page, joins, conditions, sort, filter, include)
     end
-  
+
     def self.search_by_security_group(security_group, search, page, extra_joins, extra_conditions, sort=nil, filter=nil, include=nil)
         joins = [
             'INNER JOIN security_groups_servers ON security_groups_servers.server_id = servers.id',
@@ -276,7 +278,7 @@ class Server < BaseModel
             conditions[0] << ' AND ' + extra_conditions[0];
             conditions << extra_conditions[1..-1]
         end
-  
+
         search(search, page, joins, conditions, sort, filter, include)
     end
 
@@ -304,7 +306,7 @@ class Server < BaseModel
             end
             conditions[0] = "(#{local_conditions.join(' OR ')})"
         end
-    
+
         joins = joins + extra_joins unless extra_joins.blank?
 
         unless extra_conditions.blank?
@@ -312,7 +314,7 @@ class Server < BaseModel
             conditions[0] << ' AND ' + extra_conditions[0]
             conditions << extra_conditions[1..-1]
         end
-    
+
         order = table_name()+'.name' if order.blank?
 
         options.merge!({
@@ -321,9 +323,9 @@ class Server < BaseModel
             :order => order,
         })
 
-        return options        
+        return options
     end
-    
+
     def self.per_page
         10
     end
@@ -342,7 +344,7 @@ class Server < BaseModel
     #
     def start!(count=1, options={})
         instances = []
-        
+
         begin
             instances = Ec2Adapter.run_instances(self, count, options)
         rescue Exception => e
@@ -350,10 +352,10 @@ class Server < BaseModel
             Rails.logger.error msg+"\n\t#{e.backtrace.join("\n\t")}"
             self.errors.add(:state, "#{msg}")
         end
-        
+
         return instances
     end
-    
+
     def add_user_key(user_key, server_user)
     self.instances.each do |instance|
         next if not instance.running?
@@ -382,5 +384,38 @@ class Server < BaseModel
         }
         )
     end
+    end
+
+    def server_users
+      users = {}
+      server_user_accesses.each do |sua|
+        u = sua.user
+        keys = u.user_keys
+        users[u.server_user] << u if keys.size > 0
+      end
+      users
+    end
+
+    def generate_user_data compress=false
+      if startup_script_packager == 'none'
+        return startup_script.delete("\C-M")
+      end
+
+      server_user_data = ServerUserData.new self
+      payload = server_user_data.get_payload
+
+      if compress
+        payload.gsub!(/^\s*\n/,'')
+        payload.gsub!(/^#\s+.*\n/,'')
+        loader = File.read(COMPRESSION_TEMPLATE)
+        StringIO.open(loader, 'ab') do |f|
+          gz = Zlib::GzipWriter.new(f)
+          gz.write payload
+          gz.close
+        end
+        payload = loader
+      end
+
+      payload
     end
 end
