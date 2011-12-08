cf048853cbd23fedcd5928bef648b3f548f7d281
Chris DiMartino <christopher.dimartino@nytimes.com>
2011-12-07 19:10:20 -0500

    Updates to support the user_data modifications

--- app/models/server.rb
+++ app/models/server.rb
@@ -400,22 +400,7 @@ class Server < BaseModel
       if startup_script_packager == 'none'
         return startup_script.delete("\C-M")
       end
-
       server_user_data = ServerUserData.new self
-      payload = server_user_data.get_payload
-
-      if compress
-        payload.gsub!(/^\s*\n/,'')
-        payload.gsub!(/^#\s+.*\n/,'')
-        loader = File.read(COMPRESSION_TEMPLATE)
-        StringIO.open(loader, 'ab') do |f|
-          gz = Zlib::GzipWriter.new(f)
-          gz.write payload
-          gz.close
-        end
-        payload = loader
-      end
-
-      payload
+      server_user_data.get_loader compress
     end
 end
