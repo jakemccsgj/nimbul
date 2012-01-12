308e86ee2f593bbe4edf5f1db70dc7dabc2d013b
Chris DiMartino <christopher.dimartino@nytimes.com>
2012-01-11 22:19:03 -0500

    Resolved nil CloudResource objects issue in resource_daemon

--- lib/daemons/resources_daemon.rb
+++ lib/daemons/resources_daemon.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-LOOP_SLEEP_TIME = 10
+LOOP_SLEEP_TIME = 5
 
 # You might want to change this
 ENV["RAILS_ENV"] ||= "production"
@@ -16,21 +16,25 @@ end
 while($running) do
   Rails.logger.info File.basename(__FILE__).sub('.rb','')+" daemon is still running at #{Time.now}.\n"
 
-    InstanceResource.pending.each do |ir|
-        topic = "'#{ir.cloud_resource.name}' #{ir.cloud_resource.cloud_id} [#{ir.cloud_resource.id}]"
-        topic += " to #{ir.instance.instance_id} [#{ir.instance.id}]"
-        topic += " through instance resource #{ir.id}"
-        begin
-          if ir.attach!
-              Rails.logger.info "Successfully attached #{topic}"
-          else
-              Rails.logger.error "Didn't attach #{topic}: "+ir.errors.collect{ |attr,msg| attr.humanize + ': ' + msg}.join("\n\t")
-          end
-        rescue Exception => e
-          msg = "Failed to attach #{topic}: #{e.message}"
-          Rails.logger.error msg+"\n\t#{e.backtrace.join("\n\t")}"
-        end
+  InstanceResource.pending.each do |ir|
+    if ir.cloud_resource.nil?
+      Rails.logger.warn "Error with InstanceResource: #{ir.to_s} - no CloudResource attached!"
+      next
     end
 
+    topic = "'#{ir.cloud_resource.name}' #{ir.cloud_resource.cloud_id} [#{ir.cloud_resource.id}]"
+    topic += " to #{ir.instance.instance_id} [#{ir.instance.id}]"
+    topic += " through instance resource #{ir.id}"
+    begin
+      if ir.attach!
+        Rails.logger.info "Successfully attached #{topic}"
+      else
+        Rails.logger.error "Didn't attach #{topic}: "+ir.errors.collect{ |attr,msg| attr.humanize + ': ' + msg}.join("\n\t")
+      end
+    rescue Exception => e
+      msg = "Failed to attach #{topic}: #{e.message}"
+      Rails.logger.error msg+"\n\t#{e.backtrace.join("\n\t")}"
+    end
+  end
   sleep LOOP_SLEEP_TIME
 end
