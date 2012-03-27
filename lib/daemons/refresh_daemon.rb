1056b1b11f34c594d356707f5d657df4deada7af
root <christopher.dimartino@nytimes.com>
2012-03-27 11:59:23 -0400

    Hotfixes from Vadim to fix daemons issues

--- lib/daemons/refresh_daemon.rb
+++ lib/daemons/refresh_daemon.rb
@@ -1,12 +1,11 @@
 #!/usr/bin/env ruby
 
-LOOP_SLEEP_TIME = 60
+LOOP_SLEEP_TIME = 300
 
 require 'rubygems'
 
 # You might want to change this
 ENV["RAILS_ENV"] ||= "production"
-ENV['DAEMON_SCRIPTLET'] = 'true'
 
 require File.dirname(__FILE__) + "/../../config/environment"
 load File.join(RAILS_ROOT, 'lib', 'detached_workers.rb')
@@ -29,12 +28,14 @@ Signal.trap("INT")  { shutdown }
 while($running) do
   Rails.logger.info File.basename(__FILE__, '.rb') + " daemon is still running at #{Time.now}.\n"
 
-  $accounts = ProviderAccount.find(:all, :select => 'id,name').select { |o| Ec2Adapter.get_ec2(o) }.collect { |o| o[:id].to_i }
-  $accounts.each do |account_id|
-    $manager.add_task { ProviderAccount.find_by_id(account_id).refresh }
+  ProviderAccount.all.each do |account|
+    unless Ec2Adapter.get_ec2(account).nil?
+      $manager.add_task { account.refresh }
+    end
   end
-  
-  $manager.complete_tasks  
+
+  # wait for all tasks to finish
+  $manager.complete_tasks
 
   sleep LOOP_SLEEP_TIME
 end
