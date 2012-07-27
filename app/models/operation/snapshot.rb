ccf8770fcbd0ab0f4f54786e92fc5ef98461570a
Chris DiMartino <christopher.dimartino@nytimes.com>
2012-07-27 14:23:37 -0400

    updated steps for operation snapshot to record one step per volume

--- app/models/operation/snapshot.rb
+++ app/models/operation/snapshot.rb
@@ -2,17 +2,18 @@ require "models/volume"
 require "models/ec2_adapter"
 
 class Operation::Snapshot < Operation
-  private
-  step_errors = {}
+  def initialize *args
+    @step_errors = {}
+    super(*args)
+  end
 
-  public
   def self.label
     'EBS Snapshot'
   end
 
   def self.is_schedulable?
     true
-  end  
+  end
 
   def timeout
     5.minutes
@@ -25,25 +26,25 @@ class Operation::Snapshot < Operation
     steps += post_snapshot_steps || []
     steps
   end
-  
+
   def pre_snapshot_steps() nil; end
   def post_snapshot_steps() nil; end
-  
+
   def snapshot_step
     steps = CloudVolume.find_all_by_instance_id(instance.id).collect do |vol|
       Operation::Step.new('create_ebs_snapshot') do
         snapshot = vol.snapshot!
         error = snapshot.nil? ? "There was an error creating the snapshot: #{vol.errors.collect{|attr,msg| "#{attr} - #{msg}" }.join("; ")}" \
               :                  nil
-        self.step_errors[vol.name] = error
-  
+        @step_errors[vol.name] = error
+
         self[:result_code] =  \
-          case self.step_errors.values.count(nil)
-            when self.step_errors.count then 'Success'
+          case @step_errors.values.count(nil)
+            when @step_errors.count then 'Success'
             when 0            then 'Failure'
             else              'Partial Success'
           end
-        self[:result_message] = "Created #{self.step_errors.values.count(nil)} / #{self.step_errors.count} snapshots"
+        self[:result_message] = "Created #{@step_errors.values.count(nil)} / #{@step_errors.count} snapshots"
         operation_logs << OperationLog.new(
           :step_name => "create_ebs_snapshot: #{vol.name}",
           :is_success => error.nil?,
@@ -51,10 +52,9 @@ class Operation::Snapshot < Operation
           :result_message => error.nil? ? "Created snapshot #{snapshot.name}" : error
         )
 
-        #(self.step_errors.values.count(nil) == self.step_errors.count) ? wait! : fail!
+        (@step_errors.values.count(nil) == @step_errors.count) ? proceed! : fail!
       end
     end
-    proceed! if can_proceed?
     steps
   end
 end
